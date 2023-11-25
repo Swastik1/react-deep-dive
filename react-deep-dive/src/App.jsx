a66840e5968796ed24d107551dc5835ff6f420b4
Swastik <swastikpanda32@gmail.com>
2023-11-25 21:53:56 +0530

    reading props in event handlers

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 
-export default function App() {
-  // const handleButtonClick = () => {
-  //   alert("You clicked a button!")
-  // }
+function AlertButton({ text, children }) {
   return (
-    <button onClick={() => alert('You clicked me!')}>I don't do anything</button>
+    <button onClick={() => alert(text)}>{children}</button>
+  )
+}
+
+export default function App() {
+  return (
+    <div>
+      <AlertButton text="You clicked Play button">
+        Play movie
+      </AlertButton>
+      <AlertButton text="You clicked Upload button">
+        Upload a movie
+      </AlertButton>
+    </div>
   )
 }
 
