2666fa7a06d94f69132fe8f4852dc703649569e6
Swastik <swastikpanda32@gmail.com>
2023-11-28 20:29:11 +0530

    Adding more state variables to the component

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -4,9 +4,14 @@ import './App.css'
 
 export default function App() {
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(0);
 
   function handleClick() {
     setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
@@ -16,10 +21,13 @@ export default function App() {
       <h1>
         <i>{sculpture.name}</i> by {sculpture.artist}
       </h1>
-      <p>({index + 1} of {sculptureList.length})</p>
-      <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
       <button onClick={handleClick}>Next</button>
+      <p>({index + 1} of {sculptureList.length})</p>
+      <button onClick={handleMoreClick}>{showMore ? 'Hide' : 'Show'} details</button>
+      {showMore && <p>{sculpture.description}</p>}
+      <br />
+      <br />
+      <img src={sculpture.url} alt={sculpture.alt} />
     </>
   )
 } 
