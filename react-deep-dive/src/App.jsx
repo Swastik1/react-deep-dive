9a01517cdc42062412386c170df45468361f3515
Swastik <swastikpanda32@gmail.com>
2023-12-04 21:35:20 +0530

    updating nested objects

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -4,16 +4,20 @@ import React from 'react'
 
 const Form = () => {
 
-  const [person, setPerson] = useState({
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'johndoe@gmail.com'
+  const [person, setPerson] = useState(
+    {
+    name: 'Niki de Saint Phalle',
+    artwork: {
+      title: 'Blue Nana',
+      city: 'Hamburg',
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    }
   })
 
   const handlePerson = (e) => {
     setPerson({
       ...person,
-      [e.target.name]: e.target.value
+      [e.target.item]: e.target.value
     })
   }
 
@@ -21,30 +25,39 @@ const Form = () => {
     <div style={{textAlign: 'center'}}>
       <h1>Data Form</h1>
 
-      <label>FirstName: <input
-        name="firstName"
-        value={person.firstName}
+      <label> Name: <input
+        item="name"
+        value={person.name}
         onChange={handlePerson} />
       </label>
       <br />
 
-      <label>LastName: <input
-        name="lastName"
-        value={person.lastName}
+      <label> Title: <input
+        item="title"
+        value={person.artwork.title}
         onChange={handlePerson} />
       </label>
       <br />
 
-      <label>Email: <input
-        name="email"
-        value={person.email}
+      <label> City: <input
+        item="city"
+        value={person.artwork.city}
+        onChange={handlePerson} />
+      </label>
+      <br />
+      <label> Image: <input
+        item="image"
+        value={person.artwork.image}
         onChange={handlePerson} />
       </label>
 
-      <p>{person.firstName}{" "}
-        {person.lastName}{" "}
-        ({person.email})
+      <p><h3>{person.artwork.title}</h3>{" "} by {" "}
+        <b>{person.name}</b>{" "}
+        (located in {" "}
+        {person.artwork.city}){" "}
       </p>
+
+      <img src={person.artwork.image} alt={person.artwork.ti} />
     </div>
   )
 }
