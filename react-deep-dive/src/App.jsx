a1a396e7754e1cf47d69f19c4aaa6f2084075eb1
Swastik <swastikpanda32@gmail.com>
2023-12-08 00:10:58 +0530

    updating objects inside arrays

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -1,36 +1,70 @@
 import React from 'react'
 import { useState } from 'react';
 
-let nextId = 3;
 const initialList = [
-  { id: 0, title: 'Big Bellies' },
-  { id: 1, title: 'Lunar Landscape' },
-  { id: 2, title: 'Terracotta Army' },
+  { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true },
 ];
 
-
-const List = () => {
-  const [items, setItems] = useState(initialList);
-
-  const handleClick = () => {
-    const nextItems = [...items];
-    nextItems.reverse();
-    setItems(nextItems);
-  };
-
-  const itemContent = items.map((item) => {
-    return (
-      <li key={item.id}>{item.title}</li>
-    )
-  });
+const BucketList = () => {
+  const [myList, setMyList] = useState(initialList);
+  const [yourList, setYourList] = useState(initialList);
     
+  const handleToggleMyList = (artworkId,nextSeen) => {
+    setMyList(myList.map((artwork) => {
+      if (artwork.id === artworkId) {
+        return {...artwork, seen: nextSeen}
+      } else {
+        return artwork;
+      }
+    }))
+  } 
+
+  const handleToggleYourList = (artworkId, nextSeen) => {
+    setYourList(yourList.map((artwork) => {
+      if (artwork.id === artworkId) {
+        return {...artwork, seen: nextSeen}
+      } else {
+        return artwork;
+      }
+    }))
+  }
 
   return (
     <div>
-      <button onClick={handleClick}>Reverse</button>
-      <ul>{itemContent}</ul>
+      <h1>Art Bucket List: </h1>
+      <h2>My List of art to see:</h2>
+      <ItemList artworks={myList} onToggle={handleToggleMyList} />
+      <h2>Your List of art to see:</h2>
+      <ItemList artworks={yourList} onToggle={handleToggleYourList} />
     </div>
   )
 }
 
-export default List;
+const ItemList = ({artworks,onToggle}) => {
+  return (
+    <ul>
+      {artworks.map((artwork) => {
+        return (
+          <li key={artwork.id}>
+            <label>
+              <input type="checkbox"
+                checked={artwork.seen}
+                onChange={(e) => {
+                  onToggle(
+                    artwork.id,
+                    e.target.checked
+                  )
+                }}
+              />
+              {artwork.title}
+            </label>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default BucketList;
