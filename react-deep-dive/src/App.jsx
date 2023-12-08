00272bd2d3cdcde430de2fd485d9e58874f20fcf
Swastik <swastikpanda32@gmail.com>
2023-12-08 19:36:35 +0530

    connecting event handlers to set state

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -1,26 +1,57 @@
-import './App.css';
-import Form from './Form';
-
-let statuses = [
-    'empty',
-    'typing',
-    'submitting',
-    'success',
-    'error',
-]
+import React, { useState } from 'react'
+import './App.css'
 const App = () => {
-    return (
-        <>
-            {statuses.map((status) => {
-                return (
-                    <section key={status}>
-                    <h4>Form ({status}):</h4>
-                    <Form status={status}/>
-                    </section>
-                )
-            })}
-        </>
-    )
+  const [answer, setAnswer] = useState(""); 
+  const [status, setStatus] = useState("typing");
+  const [error, setError] = useState(null);
+
+  if (status === 'success') {
+    return <h2>That's right!</h2>
+  }
+
+  const handleChange = (e) => {
+    setAnswer(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus('submitting')
+    try {
+      await submitForm(answer);
+      setStatus('success')
+    } catch (err) {
+      setStatus('typing');
+      setError(err)
+    }
+  } 
+
+  return (
+    <div>
+      <h2>City Quiz: </h2>
+      <p>In which city is there a billboard that turns air into drinkable water?</p>
+      <form onSubmit={handleSubmit}>
+        <textarea value={answer} onChange={handleChange}  disabled={ status === 'submitting'} />
+        <br />
+        <button disabled={answer.length === 0 || status === 'submitting'}>Submit</button>
+        {error !== null && 
+          <p className="Error">{error.message }</p>
+        } 
+      </form>
+    </div>
+  )
+}
+
+function submitForm(answer) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima';
+    if (shouldError) {
+      reject(new Error("Good guess but wrong answer!"))
+    } else {
+      resolve();
+    }
+    },1500)
+  })
 }
 
 export default App;
