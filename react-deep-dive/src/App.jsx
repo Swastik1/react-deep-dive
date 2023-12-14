5a711e6e7c46f74f70cbbe2e0f9bfe64a6369fd9
Swastik <swastikpanda32@gmail.com>
2023-12-14 18:06:46 +0530

    avoid redundant state

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -1,46 +1,36 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [text, setText] = useState("");
-  const [status, setStatus] = useState('typing');
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: ""
+  })
 
-  const handleChange = (e) => {
-    setText(e.target.value)
+  const handleFirstNameChange = (e) => {
+    setUser({...user, firstName: e.target.value})
   }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus('sending');
-    await sendMessage(text);
-    setStatus('sent');
+  const handleLastNameChange = (e) => {
+    setUser({...user, lastName: e.target.value})
   }
 
-  const isSending = status === 'sending';
-  const isSent = status === 'sent';
+  const fullName = user.firstName + " " + user.lastName;
 
-  if (isSent) {
-    return <h1>Thanks for the feedback!</h1>
-  }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <p>How was your stay at The Prancing Pony?</p>
-        <textarea
-          disabled={isSending}
-          value={text}
-          onChange={handleChange}
-        ></textarea>
-        <br />
-        <button disabled={isSending} >Send</button>
-        {isSending && <p>Sending ...</p>}
-      </form>
+      <h2>Let's check you in</h2>
+      <label>First Name: 
+        <input type="text" value={user.firstName} onChange={handleFirstNameChange}  />
+      </label>
+      <br />
+      <label>Last Name:
+        <input type="text" value={user.lastName} onChange={handleLastNameChange}/>
+      </label>
+      <br />
+      <label>Your ticket will be issued to: <b>{fullName}</b> </label>  
     </div>
   )
-}
-
-const sendMessage = (text) => {
-  return new Promise(resolve => setTimeout(resolve,2000))
 }
 
 export default App
