17e4a6489065521877560d524a1376177a2d2e37
Swastik <swastikpanda32@gmail.com>
2023-12-14 19:06:39 +0530

    removed the earlier duplicate state and kept essential state

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -8,15 +8,27 @@ const initialItems = [
 
 const App = () => {
   const [items, setItems] = useState(initialItems);
-  const [selectedItem, setSelectedItem] = useState(items[0]);
+  const [selectedId, setSelectedId] = useState(0);
 
-  const handleClick = (item) => {
-    setSelectedItem(item)
+  const selectedItem = items.find(item => item.id === selectedId);
+
+  const handleClick = (itemID) => {
+    setSelectedId(itemID)
+  }
+
+  const handleItemChange = (id, e) => {
+    setItems(items.map((item) => {
+      if (item.id === id) {
+        return {...item, title: e.target.value}
+      } else {
+        return item;
+      }
+    }))
   }
 
   const itemContent = Array.isArray(items) ? (items.map((item) => (
-    <li key={item.id}>{item.title} {" "}
-      <button onClick={() => handleClick(item)}>Choose</button>
+    <li key={item.id}><input value={item.title} onChange={(e) => handleItemChange(item.id,e)} />
+      <button onClick={() => handleClick(item.id)}>Choose</button>
     </li>
   ))) : (
       <div>Loading ...</div>
