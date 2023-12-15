ba9ae8797f5fe8075e235f87ecbbc71cd1071216
Swastik <swastikpanda32@gmail.com>
2023-12-15 15:45:44 +0530

    avoid deeply nested state

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -1,48 +1,33 @@
-import React, { useState } from 'react'
+import { useState } from 'react';
+import { initialTravelPlan } from './places.js';
 
-const initialItems = [
-  { title: 'pretzels', id: 0 },
-  { title: 'crispy seaweed', id: 1 },
-  { title: 'granola bar', id: 2 },
-];
-
-const App = () => {
-  const [items, setItems] = useState(initialItems);
-  const [selectedId, setSelectedId] = useState(0);
-
-  const selectedItem = items.find(item => item.id === selectedId);
-
-  const handleClick = (itemID) => {
-    setSelectedId(itemID)
-  }
-
-  const handleItemChange = (id, e) => {
-    setItems(items.map((item) => {
-      if (item.id === id) {
-        return {...item, title: e.target.value}
-      } else {
-        return item;
-      }
-    }))
-  }
-
-  const itemContent = Array.isArray(items) ? (items.map((item) => (
-    <li key={item.id}><input value={item.title} onChange={(e) => handleItemChange(item.id,e)} />
-      <button onClick={() => handleClick(item.id)}>Choose</button>
-    </li>
-  ))) : (
-      <div>Loading ...</div>
-  )
-
+function PlaceTree({ place }) {
+  const childPlaces = place.childPlaces;
   return (
-    <div>
-      <h2>What's your travel snack?</h2>
-      <ul>
-        {itemContent}
-      </ul>
-      <p>You selected { selectedItem.title}</p>
-    </div>
-  )
+    <li>
+      {place.title}
+      {childPlaces.length > 0 && (
+        <ol>
+          {childPlaces.map(place => (
+            <PlaceTree key={place.id} place={place} />
+          ))}
+        </ol>
+      )}
+    </li>
+  );
 }
 
-export default App
+export default function TravelPlan() {
+  const [plan, setPlan] = useState(initialTravelPlan);
+  const planets = plan.childPlaces;
+  return (
+    <>
+      <h2>Places to visit</h2>
+      <ol>
+        {planets.map(place => (
+          <PlaceTree key={place.id} place={place} />
+        ))}
+      </ol>
+    </>
+  );
+}
