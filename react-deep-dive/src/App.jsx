530da74e2684b7e58b4a9cd4cb8e7823ae172cd8
Swastik <swastikpanda32@gmail.com>
2023-12-18 23:07:57 +0530

    choosing right state

--- react-deep-dive/src/App.jsx
+++ react-deep-dive/src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react';
-import { initialTravelPlan } from './places.js';
+import React, { useState } from 'react'
+import { initialTravelPlan } from './places'
 
-function PlaceTree({ place }) {
-  const childPlaces = place.childPlaces;
+const TravelPlan = () => {
+  const [plan,setPlan] = useState(initialTravelPlan);
+  
+  const handleComplete = (parentId,childId) => {
+    const parent = plan[parentId];
+    const nextParent = {
+      ...plan,
+      childIds: parent.childIds.filter(id => id !== childId)
+    }
+    setPlan({
+      ...plan,
+      [parentId]: nextParent
+    })
+  }
+
+  const root = plan[0];
+  const planetIds = root.childIds;
+
+  return (
+    <div>
+      <h2>Places to visit:</h2>
+      <ol>
+        {planetIds.map(id => (
+          <PlaceTree 
+            key={id}
+            id={id}
+            parentId={0}
+            placesById={plan}
+            onComplete={handleComplete}
+          />
+        ))}
+      </ol>
+    </div>
+  )
+}
+
+const PlaceTree = ({id,parentId,placesById,onComplete}) => {
+  const place = placesById[id];
+  const childIds = place.childIds;
   return (
     <li>
       {place.title}
-      {childPlaces.length > 0 && (
-        <ol>
-          {childPlaces.map(place => (
-            <PlaceTree key={place.id} place={place} />
-          ))}
-        </ol>
-      )}
+      <button onClick={() => onComplete(parentId,id)}>Complete</button>
+      {childIds.length > 0 && 
+        <ol>{childIds.map(childId => (
+          <PlaceTree 
+            key={id}
+            id={childId}
+            parentId={id}
+            placesById={placesById}
+            onComplete={onComplete}
+          />
+        ))}</ol>
+      }
     </li>
-  );
+  )
 }
 
-export default function TravelPlan() {
-  const [plan, setPlan] = useState(initialTravelPlan);
-  const planets = plan.childPlaces;
-  return (
-    <>
-      <h2>Places to visit</h2>
-      <ol>
-        {planets.map(place => (
-          <PlaceTree key={place.id} place={place} />
-        ))}
-      </ol>
-    </>
-  );
-}
+export default TravelPlan
